9b6d9eb6417b4ff7d4a30dae5b898da81264cf0f
TAAY,Janeece Mae <147222812+janeecetaay@users.noreply.github.com>
2023-10-09 10:57:57 +0800

    Add files via upload

---
+++ CS201-JMTAAY-LAB3/script.js
@@ -0,0 +1,12 @@
+let password;
+
+if(password ===8){
+    console.log("welcome");
+}else if(password <=8){
+    console.log("password too short");
+}else if(password >=8){
+    console.log("too long password");
+    console.log("password should be 8 character");
+}else{
+    console.log("please provide a password");
+}
